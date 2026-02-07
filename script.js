92e8dd80f569074d36407dc9ae341b9da0a723ed
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-02-07 19:57:12 +0530

    loopJs

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function getGrade(score) {
-    if (score >= 90 && score <= 100) {
-        return "A+";
-    } else if (score >= 75 && score < 90) {
-        return "B";
-    } else if (score >= 60 && score < 75) {
-        return "C";
-    } else if (score >= 45 && score < 60) {
-        return "D";
-    } else if (score >= 0 && score < 45) {
-        return "Fail";
-    } else {
-        return "Invalid Marks"
-    }
- 
+// 1 t0 100 print
 
-
-
+for (let i =1; i<=100; i++){
+    console.log(i);
 }
-console.log(getGrade(91));
