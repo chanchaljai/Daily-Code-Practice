879ad1c42364bc04f3a94d8e0f283c918584436f
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-02-17 19:52:47 +0530

    imgAdd

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let a = document.querySelector("a");
-a.setAttribute("href", "httsp://www.google.com");
+let img = document.querySelector("img");
+img.setAttribute(
+    "src",
+    "https://plus.unsplash.com/premium_photo-1676923901702-fe0b69695fcc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D"
+)
