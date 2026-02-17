41412d6cd420c800360489735f834494b1b90f3c
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-02-17 20:30:26 +0530

    styjeChange

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let img = document.querySelector("img");
-img.setAttribute(
-    "src",
-    "https://plus.unsplash.com/premium_photo-1676923901702-fe0b69695fcc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyOXx8fGVufDB8fHx8fA%3D%3D"
-)
+let h1 = document.querySelector("h1");
+h1.style.color = "red";
+h1.style.backgroundColor = "gray";
