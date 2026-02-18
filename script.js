8c04c907630df4f3bba225c3c8765a755faa655c
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-02-18 15:04:19 +0530

    style hi CSS hai

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let h1 = document.querySelector("h1");
 h1.style.color = "red";
 h1.style.backgroundColor = "gray";
+// font change
+h1.style.fontFamily = "Gilroy";
+//word ke first letter Capital
+h1.style.textTransform = "Capitalize";
