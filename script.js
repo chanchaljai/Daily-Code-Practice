1c3e5c57e941e54fbd1bf446e1e5719a2e8af12b
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-02-18 15:55:40 +0530

    add/remove Class

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 let h1 = document.querySelector("h1");
-h1.style.color = "red";
-h1.style.backgroundColor = "gray";
-// font change
-h1.style.fontFamily = "Gilroy";
-//word ke first letter Capital
-h1.style.textTransform = "Capitalize";
+// add/ remove class
+h1.classList.remove("hulu");
