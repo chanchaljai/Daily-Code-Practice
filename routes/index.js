bbddb313e522b694732136e51719307ebbce71bd
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-16 14:01:05 +0530

    express practice

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,40 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
+
 
 /* GET home page. */
-router.get('/', function(req, res,) {
+router.get('/', function (req, res,) {
   res.render("index")
 });
 
-=======
+
 const userModel = require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res,) {
+router.get('/', function (req, res,) {
   req.session.ban = true;
   res.render("practice");
 });
 
-router.get("/checkban", function(req, res){
-  if(req.session.ban === true){
+router.get("/checkban", function (req, res) {
+  if (req.session.ban === true) {
     res.send("you are banned")
   }
-  else{
+  else {
     res.send("not banned")
   }
-  
+
 });
 
-router.get("/removeban", function(req, res){
-  req.session.destroy(function(err){
-    if(err) throw err;
-    res.send("ban remove");
+router.get("/removeban", function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) throw err;
+    res.send("ban remove")
   })
 
 });
 
-router.get("/alluser", async function(req, res){
+router.get("/alluser", async function (req, res) {
   const alluser = await userModel.find();
   res.send(alluser);
 });
@@ -46,5 +46,4 @@ router.get("/alluser", async function(req, res){
 // });
 
 
->>>>>>> f64fd72 (session and cookies in express generator)
 module.exports = router;
