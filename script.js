ab108faafafb756895bb3baa53680d6960e974cb
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-29 20:46:20 +0530

    blank

--- script.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector("#menu-icon");
-menu.addEventListener("click", () => {
-    const menuItems = document.querySelector("ul");
-    menuItems.classList.toggle("active");
-})
